c0c3436fda2ccafef3aad3ef857d0dc6b3472304
vmahajanam <vmahajanam@hotmail.com>
2018-10-02 15:43:51 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -11,13 +11,23 @@ library(shiny)
 # Define UI for application that displays average CO2 uptake by place of plant origin
 
 fluidPage(
-  checkboxGroupInput("variable", "Average CO2 upatke of plants originated at:(select the checkbox below)",
+
+  h4("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"),
+  
+  h4("This App gives you the mean CO2 uptake of grass plants based on their origin. The data comes from observations of an experiment conducted on grass plants. You need to select the checkbox below to get the mean CO2 uptake of plants originating from that region"),
+  h4("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"),
+  
+  checkboxGroupInput("variable", "select the checkbox",
                      c(
                        "Quebec" = 2,
                        "Mississippi" = 1)),
   
   tableOutput("data"),
-mainPanel(
-  h4("CO2 dataset:"),
+  h4("Note:"),
+  h5("The entire observations from this experiment are provided below as CO2 dataset. The column 'Type' represents the origin of the plant. The last column 'uptake' represents the CO2 uptake"),
+  h5("For more details on CO2 dataset please click the link to ",a("My presentation on reproducible pitch of this app", href="http://rpubs.com/vmahajanam/CO2App-Rep-Pitch")),
+  mainPanel(
+  h6("CO2 dataset contents:"),
   tableOutput("data1"))
+
 )
